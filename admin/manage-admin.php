b48e1994a62e3bfc2862f7eff0afc5f4cdc8f3e0
camelCasePh <camelCase.ph@gmail.com>
2022-05-02 22:18:19 +0800

    minor changes

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -3,7 +3,24 @@
         <!--menu ends here-->
         <div class="main-content">
             <div class="wrapper">
-                    <h1>Admin Page</h1>
+                    <h1>Manage admin</h1>
+                    <table>
+                        <tr>
+                            <th>S.N.</th>
+                            <th>Full name</th>
+                            <th>Username</th>
+                            <th>Actions</th>
+                        </tr>
+                        <tr>
+                            <td>1.</td>
+                            <td>Bryan Ladion</td>
+                            <td>Kikitiw</td>
+                            <td>
+                                Update Admin
+                                Delete Admin
+                            </td>
+                        </tr>
+                    </table>
     
             </div>
         </div>
--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,6 +1,6 @@
 <div class="footer text-align-center">
             <div class="wrapper">
-                 <p>All rights reserved. Harris Food Developed by HHB Team. <a href="https://www.facebook.com/HHB-Team-107144751990798">Visit us here.</p>
+                 <p>All rights reserved. Harris Food Developed by HHB Team. <a href="https://www.facebook.com/HHB-Team-107144751990798" target="blank">Visit us here.</p>
             </div>
            
         </div>
