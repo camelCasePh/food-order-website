6b80e0799c10d53ccf2785cdfe27e97fce531652
camelCasePh <camelCase.ph@gmail.com>
2022-05-02 18:55:33 +0800

    integrating php and mysql and adding admin panel

---
+++ admin/manage-category.php
@@ -0,0 +1,13 @@
+
+        <?php include('partials/menu.php')?>
+        <!--menu ends here-->
+        <div class="main-content">
+            <div class="wrapper">
+                    <h1>Manage Categories</h1>
+    
+            </div>
+        </div>
+            <!--main-content ends here-->
+
+            <?php include('partials/footer.php')?>
+      
